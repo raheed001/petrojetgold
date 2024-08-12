eabd9637da6dff2d359c985c80297e009091216c
raheed001 <rah004@chowgules.ac.iin>
2024-08-12 15:27:22 +0400

    chnaged affiliation positom

--- components/Affiliation.tsx
+++ components/Affiliation.tsx
@@ -5,20 +5,21 @@ import Image from 'next/image';
 
 const Affiliation = () => {
   return (
-    <div className="p-4 md:p-8 rounded-lg shadow-md mx-auto max-w-screen-lg">
+    <div className="p-4 md:p-8 rounded-lg shadow-md mx-auto ">
       <h1 className="text-center text-2xl font-bold mb-6">We are a Proud Group of Companies</h1>
-      <div className="flex justify-center mb-6">
-        <div className="w-1/2 md:w-1/4 rounded-lg shadow-lg relative">
-          <Image 
-            src="/images/ahs.png"
-            alt="Qasr Al Ainaya Logo"
-            layout="fill"
-            objectFit="contain"
-            className="rounded-lg"
-          />
+      
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-8 gap-4">
+        <div className="flex items-center justify-center">
+          <div className="relative w-full h-32">
+            <Image 
+              src="/images/ahs.png"
+              alt="AHS"
+              layout="fill"
+              objectFit="contain"
+              className="rounded-lg shadow-lg"
+            />
+          </div>
         </div>
-      </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-7 gap-4">
         <div className="flex items-center justify-center">
           <div className="relative w-full h-32">
             <Image 
@@ -96,7 +97,6 @@ const Affiliation = () => {
             />
           </div>
         </div>
-        {/* Add more logos as needed */}
       </div>
     </div>
   );
